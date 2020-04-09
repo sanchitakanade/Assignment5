a3df98a4ecafd840ee87ad4619a03e66a288bf50
sanchita kanade <sanchitamystudies@gmail.com>
2020-04-08 18:04:37 -0700

    made an update

---
+++ ui/src/ProductReport.jsx
@@ -0,0 +1,10 @@
+/* eslint linebreak-style: ["error", "windows"] */
+import React from 'react';
+
+export default function IssueReport() {
+  return (
+    <div>
+      <h2>This is a placeholder for the Issue Report</h2>
+    </div>
+  );
+}
